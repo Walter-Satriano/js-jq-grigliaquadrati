445943151e6362daf31f558cabc04bef718703c0
Walter Satriano <walter.satri@gmail.com>
2019-07-17 17:28:19 +0200

    aggiunta prova per cambiare colore ai quadratini

--- main.js
+++ main.js
@@ -7,9 +7,18 @@ se cliccati diventano rossi, gli altri diventano verdi*/
 $(document).ready(function(){
 
 
+  var redSquare = $(".square-1, .square-5, .square-12, .square-15, .square-19, .square-23, .square-28, .square-32, .square-36, .square-45, .square-52, .square-56, .square-59, .square-62, .square-64");
+
+  // funzione per far diventare i quadrati scelti di colore rosso
+  redSquare.click(function() {
+    $(this).addClass("square_red");
+  });
 
 
-
+  // TO DO: cercare soluzione per i quadrati di colore verde
+  $(".square-2").click(function() {
+    $(this).addClass("square_green");
+  });
 
 
 });
